13c8f55ec09a758e93b13c9307a0a14c1ea1e6a9
Denny Rodrigues <denny.rodrigues.carmo@gmail.com>
2022-12-05 12:49:06 -0300

    Add first logic

--- ios/RNNativeLogs.podspec
+++ ios/RNNativeLogs.podspec
@@ -6,7 +6,7 @@ Pod::Spec.new do |s|
   s.description  = <<-DESC
                   RNNativeLogs
                    DESC
-  s.homepage     = ""
+  s.homepage     = "https://github.com/V1pi/native-logs#readme-+"
   s.license      = "MIT"
   # s.license      = { :type => "MIT", :file => "FILE_LICENSE" }
   s.author             = { "author" => "viniciusspicanco@gmail.com" }
--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,13 @@
+import { NativeModules } from "react-native";
+
+
+export const { RNNativeLogs } = NativeModules;
+
 export const NativeLogs = {
-  getLogs: (identifier: string) => {
-    console.info(identifier);
+  getLogs: async (identifier: string): Promise<string[] | null> => {
+    return RNNativeLogs.readOutputLogs(identifier);
   },
-  redirectLogs: (identifier: string) => {
-    console.info(identifier);
-  }
+  redirectLogs: async (identifier: string): Promise<void> => {
+     return RNNativeLogs.setUpRedirectLogs(identifier);
+  },
 };
