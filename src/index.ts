2326eb2d17340300fb5bd84e52eaa9b90f09b0b8
Denny Rodrigues <denny.rodrigues.carmo@gmail.com>
2022-12-05 18:16:14 -0300

    Add some logs and fix iOS Bridge

--- src/index.ts
+++ src/index.ts
@@ -3,11 +3,14 @@ import { NativeModules } from "react-native";
 
 export const { RNNativeLogs } = NativeModules;
 
+
 export const NativeLogs = {
   getLogs: async (identifier: string): Promise<string[] | null> => {
+    console.log("getLogs");
     return RNNativeLogs.readOutputLogs(identifier);
   },
   redirectLogs: async (identifier: string): Promise<void> => {
+        console.log("redirectLogs");
      return RNNativeLogs.setUpRedirectLogs(identifier);
   },
 };
