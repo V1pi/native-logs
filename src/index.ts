54eac227fd926f7f4ee9115e0d6d944c72fea4bc
Denny Rodrigues <denny.rodrigues.carmo@gmail.com>
2022-12-06 09:27:51 -0300

    Remove logs

--- src/index.ts
+++ src/index.ts
@@ -6,11 +6,9 @@ export const { RNNativeLogs } = NativeModules;
 
 export const NativeLogs = {
   getLogs: async (identifier: string): Promise<string[] | null> => {
-    console.log("getLogs");
     return RNNativeLogs.readOutputLogs(identifier);
   },
   redirectLogs: async (identifier: string): Promise<void> => {
-        console.log("redirectLogs");
      return RNNativeLogs.setUpRedirectLogs(identifier);
   },
 };
