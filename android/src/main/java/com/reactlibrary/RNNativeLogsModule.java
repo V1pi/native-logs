0c3fb617f5484952c1bb18f4664f099b0e8e8fe0
DennyRodrigues <denny.rodrigues.carmo@gmail.com>
2022-12-13 12:33:41 -0300

    Redirect logs using pid

--- android/src/main/java/com/reactlibrary/RNNativeLogsModule.java
+++ android/src/main/java/com/reactlibrary/RNNativeLogsModule.java
@@ -25,6 +25,7 @@ import com.facebook.react.bridge.ReactMethod;
 import com.facebook.react.bridge.Promise;
 import com.facebook.react.bridge.WritableArray;
 import com.facebook.react.bridge.WritableNativeArray;
+
 public class RNNativeLogsModule extends ReactContextBaseJavaModule {
     private final ReactApplicationContext reactContext;
 
@@ -38,49 +39,36 @@ public class RNNativeLogsModule extends ReactContextBaseJavaModule {
         return "RNNativeLogs";
     }
 
-    @ReactMethod
-    public void setUpRedirectLogs(String fileIdentifier, final Promise promise) {
-        try {
-            File path = this.reactContext.getFilesDir();
-            String pathname = path + "/"  + fileIdentifier + ".txt";
-            File file = new File(pathname);
-            file.createNewFile();
-            Log.e("redirectLogs", pathname);
-            Runtime.getRuntime().exec("logcat -v time -f " + pathname + " -s vouchkeymanuap");
-            promise.resolve(true);
-        } catch (IOException e) {
-            promise.reject("Error when redirecting logs",e);
-            Log.e("LOGTAG", "IOException executing logcat command.", e);
-        }
+    private File getFile(String fileIdentifier){
+        File path = this.reactContext.getFilesDir();
+        String pathname = path + "/"  + fileIdentifier + ".txt";
+        File file = new File(pathname);
+        return file;
     }
+
+     @ReactMethod
+     public void setUpRedirectLogs(String fileIdentifier, final Promise promise) {
+         try {
+             File file = getFile(fileIdentifier);
+             file.createNewFile();
+             int pid = android.os.Process.myPid();
+             Runtime.getRuntime().exec("logcat -v time -f " + file + " --pid=" + pid);
+             promise.resolve(true);
+         } catch (IOException e) {
+             promise.reject("Error when redirecting logs", String.valueOf(e));
+             Log.e("setUpRedirectLogs", "IOException executing logcat command.", e);
+         }
+     }
 
     @ReactMethod
     public void readOutputLogs(String fileIdentifier, final Promise promise) {
-        //Get the text file
-        File path = this.reactContext.getFilesDir();
-        String pathname = path + "/" + fileIdentifier + ".txt";
-        File file = new File(pathname);
-
-        //Read text from file
-        StringBuilder textArray = new StringBuilder();
-        WritableArray writableArray = new WritableNativeArray();
-
         try {
-//            // A DIFFERENT WAY TO GE THE TEXT Logs
-//            if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
-//                String content = new String(Files.readAllBytes(Paths.get(pathname)));
-//                String[] linesArray = content.split("\\r?\\n");
-//                for(int i = 0; i < linesArray.length; i++)
-//                {
-//                    writableArray.pushString(linesArray[i]);
-//                }
-//            }
+            File file = getFile(fileIdentifier);
+            WritableArray writableArray = new WritableNativeArray();
 
-            // FIRST WAY way 
+            // FIRST WAY way
             FileReader fileReader = new FileReader(file);
             BufferedReader br = new BufferedReader(fileReader);
-            Log.e(" BufferedReader;", " bBufferedReader;");
-
             boolean done = false;
 
             while (!done) {
