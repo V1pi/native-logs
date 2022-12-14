6d99b9a3706bfaf799c600f0c323e3d78978f166
DennyRodrigues <denny.rodrigues.carmo@gmail.com>
2022-12-14 08:13:21 -0300

    Remove unnecessary imports and lint code

--- android/src/main/java/com/reactlibrary/RNNativeLogsModule.java
+++ android/src/main/java/com/reactlibrary/RNNativeLogsModule.java
@@ -1,23 +1,9 @@
-
 package com.reactlibrary;
-
-import android.util.Log;
-import android.content.Context;
 
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Paths;
-import java.security.MessageDigest;
-import java.security.NoSuchAlgorithmException;
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.UUID;
-import java.util.concurrent.CompletableFuture;
-import java.util.concurrent.TimeUnit;
-import java.util.stream.Collectors;
 
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
@@ -39,26 +25,25 @@ public class RNNativeLogsModule extends ReactContextBaseJavaModule {
         return "RNNativeLogs";
     }
 
-    private File getFile(String fileIdentifier){
+    private File getFile(String fileIdentifier) {
         File path = this.reactContext.getFilesDir();
-        String pathname = path + "/"  + fileIdentifier + ".txt";
+        String pathname = path + "/" + fileIdentifier + ".txt";
         File file = new File(pathname);
         return file;
     }
 
-     @ReactMethod
-     public void setUpRedirectLogs(String fileIdentifier, final Promise promise) {
-         try {
-             File file = getFile(fileIdentifier);
-             file.createNewFile();
-             int pid = android.os.Process.myPid();
-             Runtime.getRuntime().exec("logcat -v time -f " + file + " --pid=" + pid);
-             promise.resolve(true);
-         } catch (IOException e) {
-             promise.reject("Error when redirecting logs", String.valueOf(e));
-             Log.e("setUpRedirectLogs", "IOException executing logcat command.", e);
-         }
-     }
+    @ReactMethod
+    public void setUpRedirectLogs(String fileIdentifier, final Promise promise) {
+        try {
+            File file = getFile(fileIdentifier);
+            file.createNewFile();
+            int pid = android.os.Process.myPid();
+            Runtime.getRuntime().exec("logcat -v time -f " + file + " --pid=" + pid);
+            promise.resolve(true);
+        } catch (IOException e) {
+            promise.reject("Error when redirecting logs", String.valueOf(e));
+        }
+    }
 
     @ReactMethod
     public void readOutputLogs(String fileIdentifier, final Promise promise) {
@@ -82,9 +67,10 @@ public class RNNativeLogsModule extends ReactContextBaseJavaModule {
 
             promise.resolve(writableArray);
         } catch (IOException e) {
-            Log.e("ERROR", String.valueOf(e), e);
-            promise.reject("Error when reading logs", e);
+            promise.reject("Error when reading logs", String.valueOf(e));
         }
-    };
+    }
+
+    ;
 
 }
