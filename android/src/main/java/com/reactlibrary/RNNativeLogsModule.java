a43b64fb9d18515fb846ca3736d5e3cd8828d23c
doug821 <dougsilva821@gmail.com>
2023-04-10 15:01:35 -0300

    feat: Add filtering functionality to Android logs
    This commit adds filtering functionality to the `readOutputLogs` method in the Android native logs module. The new version of the method now accepts an optional array of tags to ignore when reading logs from the file.
    
    The commit also updates the `redirectLogs` method to accept a shortened version of the log level string instead, allowing also to add filters based on the log level priority.
    
    Both changes are specific to Android devices and improve the functionality of the native logs module.

--- android/src/main/java/com/reactlibrary/RNNativeLogsModule.java
+++ android/src/main/java/com/reactlibrary/RNNativeLogsModule.java
@@ -10,6 +10,7 @@ import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
 import com.facebook.react.bridge.Promise;
+import com.facebook.react.bridge.ReadableArray;
 import com.facebook.react.bridge.WritableArray;
 import com.facebook.react.bridge.WritableNativeArray;
 
@@ -29,37 +30,52 @@ public class RNNativeLogsModule extends ReactContextBaseJavaModule {
     private File getFile(String fileIdentifier) {
         File path = this.reactContext.getFilesDir();
         String pathname = path + "/" + fileIdentifier + ".txt";
-        File file = new File(pathname);
-        return file;
+        return new File(pathname);
     }
 
     @ReactMethod
-    public void setUpRedirectLogs(String fileIdentifier, final Promise promise) {
+    public void setUpRedirectLogs(String fileIdentifier, String logLevel, final Promise promise) {
         try {
             File file = getFile(fileIdentifier);
             file.createNewFile();
             int pid = android.os.Process.myPid();
-            Runtime.getRuntime().exec("logcat -v time -f " + file + " --pid=" + pid);
+            StringBuilder commandBuilder = new StringBuilder();
+            commandBuilder.append("logcat -v time -f ").append(file.getAbsolutePath()).append(" --pid=").append(pid);
+
+            if (logLevel != null) {
+                String packagePrefix = reactContext.getPackageName() + ":";
+                commandBuilder.append(" *:").append(logLevel).append(" ").append(packagePrefix).append(logLevel);
+            }
+
+            String command = commandBuilder.toString();
+            Runtime.getRuntime().exec(command);
             promise.resolve(true);
         } catch (IOException e) {
             promise.reject("Error when redirecting logs", String.valueOf(e));
         }
     }
 
+    private boolean isIgnoredTag(String line, ReadableArray tags) {
+        for (int i = 0; i < tags.size(); i++) {
+            String tag = tags.getString(i);
+            if (line.contains(tag)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
     @ReactMethod
-    public void readOutputLogs(String fileIdentifier, final Promise promise) {
+    public void readOutputLogs(String fileIdentifier, ReadableArray tags, final Promise promise) {
         try {
             File file = getFile(fileIdentifier);
             WritableArray writableArray = new WritableNativeArray();
             FileReader fileReader = new FileReader(file);
             BufferedReader br = new BufferedReader(fileReader);
-            boolean done = false;
 
-            while (!done) {
-                final String line = br.readLine();
-                done = (line == null);
-
-                if (line != null) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                if (!isIgnoredTag(line, tags)) {
                     writableArray.pushString(line);
                 }
             }
