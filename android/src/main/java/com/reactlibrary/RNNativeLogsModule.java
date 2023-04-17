9c8ed65895d014567b3edb801987747d8bb68ce7
Herson Douglas <55238304+Doug821@users.noreply.github.com>
2023-04-17 13:06:36 -0300

    feat: add filtering and log priority functionality to Android native logs (#4)
    * feat: Add filtering functionality to Android logs
    
    This commit adds filtering functionality to the `readOutputLogs` method in the Android native logs module. The new version of the method now accepts an optional array of tags to ignore when reading logs from the file.
    
    The commit also updates the `redirectLogs` method to accept a shortened version of the log level string instead, allowing also to add filters based on the log level priority.
    
    Both changes are specific to Android devices and improve the functionality of the native logs module.
    
    * chore: add method overloading for logging functionality
    
    This commit adds method overloading for logging functionality. The overloading was introduced in two new methods: `setUpRedirectLogs` and `readOutputLogs`.
    
    These methods provide the same functionality as their counterparts but with additional parameters that allow more control over the logging process.
    
    This overloading was added to ensure backward compatibility with iOS apps and prevent breaking changes.
    
    * chore: add method overloading and platform checks
    
    * fix: add filtering tags as optional parameter for getLogs
    
    * chore: add missing types
    
    * chore: add optional parameters type description and simplify conditions.

--- android/src/main/java/com/reactlibrary/RNNativeLogsModule.java
+++ android/src/main/java/com/reactlibrary/RNNativeLogsModule.java
@@ -10,6 +10,7 @@ import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
 import com.facebook.react.bridge.Promise;
+import com.facebook.react.bridge.ReadableArray;
 import com.facebook.react.bridge.WritableArray;
 import com.facebook.react.bridge.WritableNativeArray;
 
@@ -29,17 +30,25 @@ public class RNNativeLogsModule extends ReactContextBaseJavaModule {
     private File getFile(String fileIdentifier) {
         File path = this.reactContext.getFilesDir();
         String pathname = path + "/" + fileIdentifier + ".txt";
-        File file = new File(pathname);
-        return file;
+        return new File(pathname);
     }
 
     @ReactMethod
-    public void setUpRedirectLogs(String fileIdentifier, final Promise promise) {
+    public void setUpRedirectLogs(String fileIdentifier, String logLevel, final Promise promise) {
         try {
             File file = getFile(fileIdentifier);
             file.createNewFile();
             int pid = android.os.Process.myPid();
-            Runtime.getRuntime().exec("logcat -v time -f " + file + " --pid=" + pid);
+            StringBuilder commandBuilder = new StringBuilder();
+            commandBuilder.append("logcat -v time -f ").append(file.getAbsolutePath()).append(" --pid=").append(pid);
+
+            if (logLevel != null) {
+                String packagePrefix = reactContext.getPackageName() + ":";
+                commandBuilder.append(" *:").append(logLevel);
+            }
+
+            String command = commandBuilder.toString();
+            Runtime.getRuntime().exec(command);
             promise.resolve(true);
         } catch (IOException e) {
             promise.reject("Error when redirecting logs", String.valueOf(e));
@@ -47,19 +56,31 @@ public class RNNativeLogsModule extends ReactContextBaseJavaModule {
     }
 
     @ReactMethod
-    public void readOutputLogs(String fileIdentifier, final Promise promise) {
+    public void setUpRedirectLogs(String fileIdentifier, final Promise promise) {
+        setUpRedirectLogs(fileIdentifier, null, promise);
+    }
+
+    private boolean isIgnoredTag(String line, ReadableArray tags) {
+        for (int i = 0; i < tags.size(); i++) {
+            String tag = tags.getString(i);
+            if (line.contains(tag)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    @ReactMethod
+    public void readOutputLogs(String fileIdentifier, ReadableArray tags, final Promise promise) {
         try {
             File file = getFile(fileIdentifier);
             WritableArray writableArray = new WritableNativeArray();
             FileReader fileReader = new FileReader(file);
             BufferedReader br = new BufferedReader(fileReader);
-            boolean done = false;
 
-            while (!done) {
-                final String line = br.readLine();
-                done = (line == null);
-
-                if (line != null) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                if (tags == null || !isIgnoredTag(line, tags)) {
                     writableArray.pushString(line);
                 }
             }
@@ -70,5 +91,10 @@ public class RNNativeLogsModule extends ReactContextBaseJavaModule {
         } catch (IOException e) {
             promise.reject("Error when reading logs", String.valueOf(e));
         }
+    }
+
+    @ReactMethod
+    public void readOutputLogs(String fileIdentifier, final Promise promise) {
+        readOutputLogs(fileIdentifier, null, promise);
     }
 }
--- src/index.ts
+++ src/index.ts
@@ -1,25 +1,36 @@
-import { NativeModules } from "react-native";
+import { NativeModules, Platform } from "react-native";
 
 const { RNNativeLogs } = NativeModules;
 
+type LogLevel = "D" | "I" | "W" | "E" | "F";
+
 export const NativeLogs = {
   currentLogsIndex: {} as { [key: string]: number },
-  
+
   /** Redirects the native logs from the console to a given file.
     * @param {string} identifier - The name of the file to write the logs to.
+    * @param {LogLevel | undefined} logLevel - [ANDROID only] The log level to redirect. Can be one of: "D" for debug, "I" for info, "W" for warn, "E" for error, "F" for fatal.
     * @returns {Promise<void>} - A promise that resolves when the logs are redirected.
   */
-  async redirectLogs(identifier: string): Promise<void> {
+  async redirectLogs(identifier: string, logLevel?: LogLevel): Promise<void> {
     this.currentLogsIndex[identifier] = 0;
-    await RNNativeLogs.setUpRedirectLogs(identifier);
+    if (Platform.OS === "ios" || !logLevel) {
+      await RNNativeLogs.setUpRedirectLogs(identifier);
+    } else {
+      await RNNativeLogs.setUpRedirectLogs(identifier, logLevel);
+    }
   },
 
   /** Gets all new logs since the last call to this function.
     * @param {string} identifier - The name of the file to read the logs to.
+    * @param {string[] | undefined} tags - [ANDROID only] An array of tags to exclude from the logs. Only logs that do not match these tags will be redirected.
     * @returns {Promise<string[] | null>} - A promise that resolves with the new logs or null if there are no logs.
   */
-  async getNewLogs(identifier: string): Promise<string[] | null> {
-    const allNativeLogs = await RNNativeLogs.readOutputLogs(identifier);
+  async getNewLogs(identifier: string, tags?: string[]): Promise<string[] | null> {
+    const allNativeLogs = Platform.OS === "ios" || !tags ?
+      await RNNativeLogs.readOutputLogs(identifier) :
+      await RNNativeLogs.readOutputLogs(identifier, tags);
+
     const arrayWithNewlogs = allNativeLogs.slice(this.currentLogsIndex[identifier] || 0);
     this.currentLogsIndex[identifier] = Math.max(0, allNativeLogs.length);
     if (arrayWithNewlogs.length < 1) {
@@ -27,13 +38,16 @@ export const NativeLogs = {
     }
     return arrayWithNewlogs;
   },
-  
+
   /** Gets all logs from the given file.
     * @param {string} identifier - The name of the file to read the logs to.
+    * @param {string[] | undefined} tags - [ANDROID only] An array of tags to exclude from the logs. Only logs that do not match these tags will be redirected.
     * @returns {Promise<string[] | null>} - A promise that resolves with the logs or null if there are no logs.
   */
-  async getLogs(identifier: string): Promise<string[] | null> {
-    const allNativeLogs = await RNNativeLogs.readOutputLogs(identifier);
+  async getLogs(identifier: string, tags?: string[]): Promise<string[] | null> {
+    const allNativeLogs = Platform.OS === "ios" || !tags ?
+      await RNNativeLogs.readOutputLogs(identifier) :
+      await RNNativeLogs.readOutputLogs(identifier, tags);
     if (allNativeLogs.length < 1) {
       return null;
     }
